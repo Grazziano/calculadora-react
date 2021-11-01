c1f3047b71f97b36fd76e30405115a0a525a4946
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-11-01 20:18:40 -0300

    Criando a interface

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,12 +1,87 @@
-import React from "react";
+import React, { Component } from "react";
+import { render } from "react-dom";
 import '../css/Calculadora.css';
+import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
-function Calculadora() {
-  return (
-    <div>
-      <h1>Calculadora</h1>
-    </div>
-  );
+class Calculadora extends Component {
+  render() {
+    return (
+      <section className="calc-section">
+        <div className="jumbotron">
+          <Container>
+            <Row>
+              <Col xs="3">
+                <Button variant="warning">C</Button>
+              </Col>
+              <Col xs="9">
+                <Form.Control type="text" name="txtNumeros" className="text-right" readOnly="readonly" />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Button variant="light">7</Button>
+              </Col>
+              <Col>
+                <Button variant="light">8</Button>
+              </Col>
+              <Col>
+                <Button variant="light">9</Button>
+              </Col>
+              <Col>
+                <Button variant="info">/</Button>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Button variant="light">4</Button>
+              </Col>
+              <Col>
+                <Button variant="light">5</Button>
+              </Col>
+              <Col>
+                <Button variant="light">6</Button>
+              </Col>
+              <Col>
+                <Button variant="info">*</Button>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Button variant="light">3</Button>
+              </Col>
+              <Col>
+                <Button variant="light">2</Button>
+              </Col>
+              <Col>
+                <Button variant="light">1</Button>
+              </Col>
+              <Col>
+                <Button variant="info">-</Button>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Button variant="light">0</Button>
+              </Col>
+              <Col>
+                <Button variant="light">.</Button>
+              </Col>
+              <Col>
+                <Button variant="success">=</Button>
+              </Col>
+              <Col>
+                <Button variant="info">+</Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Calculadora;
