cbcfb5b7810046944ebc28369f4718c57b11bcb5
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-11-01 16:38:39 -0300

    Criando componente principal

---
+++ src/components/Calculadora.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../css/Calculadora.css';
+
+function Calculadora() {
+  return (
+    <div>
+      <h1>Calculadora</h1>
+    </div>
+  );
+}
+
+export default Calculadora;
