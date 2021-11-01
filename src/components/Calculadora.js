945bef93ab0de66a56bb7f9fdeeaf2266bfc8777
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-11-01 20:34:53 -0300

    Implementando o campo numérico

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,87 +1,96 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import '../css/Calculadora.css';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
-class Calculadora extends Component {
-  render() {
-    return (
-      <section className="calc-section">
-        <div className="jumbotron">
-          <Container>
-            <Row>
-              <Col xs="3">
-                <Button variant="warning">C</Button>
-              </Col>
-              <Col xs="9">
-                <Form.Control type="text" name="txtNumeros" className="text-right" readOnly="readonly" />
-              </Col>
-            </Row>
+function Calculadora() {
 
-            <Row>
-              <Col>
-                <Button variant="light">7</Button>
-              </Col>
-              <Col>
-                <Button variant="light">8</Button>
-              </Col>
-              <Col>
-                <Button variant="light">9</Button>
-              </Col>
-              <Col>
-                <Button variant="info">/</Button>
-              </Col>
-            </Row>
+  const [txtNumeros, setTxtNumeros] = useState('0');
 
-            <Row>
-              <Col>
-                <Button variant="light">4</Button>
-              </Col>
-              <Col>
-                <Button variant="light">5</Button>
-              </Col>
-              <Col>
-                <Button variant="light">6</Button>
-              </Col>
-              <Col>
-                <Button variant="info">*</Button>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                <Button variant="light">3</Button>
-              </Col>
-              <Col>
-                <Button variant="light">2</Button>
-              </Col>
-              <Col>
-                <Button variant="light">1</Button>
-              </Col>
-              <Col>
-                <Button variant="info">-</Button>
-              </Col>
-            </Row>
-
-            <Row>
-              <Col>
-                <Button variant="light">0</Button>
-              </Col>
-              <Col>
-                <Button variant="light">.</Button>
-              </Col>
-              <Col>
-                <Button variant="success">=</Button>
-              </Col>
-              <Col>
-                <Button variant="info">+</Button>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </section>
-    );
+  function adicionarNumero(numero) {
+    setTxtNumeros(txtNumeros + numero);
   }
+
+  function definirOperacao(operacao) {
+    setTxtNumeros(operacao);
+  }
+
+  return (
+    <section className="calc-section">
+      <div className="jumbotron">
+        <Container>
+          <Row>
+            <Col xs="3">
+              <Button variant="warning">C</Button>
+            </Col>
+            <Col xs="9">
+              <Form.Control type="text" name="txtNumeros" className="text-right" readOnly="readonly" value={ txtNumeros } />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('7') }>7</Button>
+            </Col>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('8') }>8</Button>
+            </Col>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('9') }>9</Button>
+            </Col>
+            <Col>
+              <Button variant="info" onClick={ () => definirOperacao('/') }>/</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('4') }>4</Button>
+            </Col>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('5') }>5</Button>
+            </Col>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('6') }>6</Button>
+            </Col>
+            <Col>
+              <Button variant="info" onClick={ () => definirOperacao('*') }>*</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('1') }>1</Button>
+            </Col>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('2') }>2</Button>
+            </Col>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('3') }>3</Button>
+            </Col>
+            <Col>
+              <Button variant="info" onClick={ () => definirOperacao('-') }>-</Button>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <Button variant="light" onClick={ () => adicionarNumero('0') }>0</Button>
+            </Col>
+            <Col>
+              <Button variant="light">.</Button>
+            </Col>
+            <Col>
+              <Button variant="success">=</Button>
+            </Col>
+            <Col>
+              <Button variant="info" onClick={ () => definirOperacao('+') }>+</Button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </section>
+  );
 }
 
 export default Calculadora;
