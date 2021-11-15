88bc95966dd2a507136ceb975f747f53967f455e
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-11-15 01:29:12 -0300

    Usando servico da calculadora no componente principal

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -1,18 +1,61 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import CalculadoraService from '../services/Calculadora.service';
 import '../css/Calculadora.css';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 
 function Calculadora() {
 
+  const [calcular, concatenarNumero, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
+
   const [txtNumeros, setTxtNumeros] = useState('0');
+  const [numero1, setNumero1] = useState('0');
+  const [numero2, setNumero2] = useState(null);
+  const [operacao, setOperacao] = useState(null);
 
   function adicionarNumero(numero) {
-    setTxtNumeros(txtNumeros + numero);
+    let resultado;
+
+    if (operacao === null) {
+      resultado = concatenarNumero(numero1, numero);
+      setNumero1(resultado);
+    } else {
+      resultado = concatenarNumero(numero2, numero);
+      setNumero2(resultado);
+    }
+
+    setTxtNumeros(resultado);
   }
 
-  function definirOperacao(operacao) {
-    setTxtNumeros(operacao);
+  function definirOperacao(op) {
+    // apenas define a operacao caso ela nao exista
+    if (operacao === null) {
+      setOperacao(op);
+      return;
+    }
+    // caso a operacao estiver definida e numero 2 selecionado, realiza o calculo da operacao
+    if (numero2 !== null) {
+      const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+      setOperacao(op);
+      setNumero1(resultado.toString());
+      setNumero2(null);
+      setTxtNumeros(resultado.toString());
+    }
+  }
+
+  function acaoCalcular() {
+    if (numero2 === null) {
+      return;
+    }
+    const resultado = calcular(parseFloat(numero1), parseFloat(numero2), operacao);
+    setTxtNumeros(resultado);
+  }
+
+  function limpar() {
+    setTxtNumeros('0');
+    setNumero1('0');
+    setNumero2(null);
+    setOperacao(null);
   }
 
   return (
@@ -21,7 +64,7 @@ function Calculadora() {
         <Container>
           <Row>
             <Col xs="3">
-              <Button variant="warning">C</Button>
+              <Button variant="warning" onClick={ limpar }>C</Button>
             </Col>
             <Col xs="9">
               <Form.Control type="text" name="txtNumeros" className="text-right" readOnly="readonly" value={ txtNumeros } />
@@ -78,10 +121,10 @@ function Calculadora() {
               <Button variant="light" onClick={ () => adicionarNumero('0') }>0</Button>
             </Col>
             <Col>
-              <Button variant="light">.</Button>
+              <Button variant="light" onClick={ () => adicionarNumero('.') }>.</Button>
             </Col>
             <Col>
-              <Button variant="success">=</Button>
+              <Button variant="success" onClick={ acaoCalcular }>=</Button>
             </Col>
             <Col>
               <Button variant="info" onClick={ () => definirOperacao('+') }>+</Button>
